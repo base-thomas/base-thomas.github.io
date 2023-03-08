fce5a91617b8b92fb485dde448d037af36818f7f
base-thomas <treynolds416@comcast.net>
2023-03-07 21:13:11 -0700

    Update CCWRAI.js

--- CCWRAI.js
+++ CCWRAI.js
@@ -420,7 +420,7 @@ Game.registerMod('CCWRAI',{
 
 			to = setTimeout(() => {this.continueRun()}, tickRate);
 			const qa = reward + discountRate * this.predict(this.getState()).max().dataSync();
-			const x = tf.tensor2d(state, [1, numStates]);
+			const x = state//tf.tensor2d(state, [1, numStates]);
 			const y = tf.tensor2d(qa, [1, numActions]);
 			await this.network.fit(x, y);
 			qa.dispose();
